b7d40c794778871ac6b95df8ebc7b14fc805596e
Phil Kedy <phil.kedy@gmail.com>
2020-10-26 20:18:32 -0400

    Refactoring (#7)
    * Misc fixes
    
    * Splitting up msgpack.ts into one class per file.  Added test generously provided by @dOrgJelli.
    
    * Adding ignored nested objects to the test.
    
    * Fixing bin length off by 1
    
    Co-authored-by: Phil Kedy <philip.kedy@capitalone.com>

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,6 @@
-export * from "./msgpack";
+export * from "./decoder";
+export * from "./writer";
+export * from "./encoder";
+export * from "./sizer";
 export * from "./value";
 export * from "./datareader";
