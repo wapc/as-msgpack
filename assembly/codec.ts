85d804f025f00da789f9ce047ff0f7fd1e5d2837
Phil Kedy <phil.kedy@gmail.com>
2020-10-30 09:58:37 -0400

    Adding Codec interface

--- assembly/codec.ts
+++ assembly/codec.ts
@@ -3,5 +3,5 @@ import { Writer } from "./writer";
 
 export interface Codec {
   decode(decoder: Decoder): void;
-  encoder(encoder: Writer): void;
+  encode(encoder: Writer): void;
 }
--- src/codec.d.ts
+++ src/codec.d.ts
@@ -3,5 +3,5 @@ import { Writer } from "./writer";
 
 export declare interface Codec {
   decode(decoder: Decoder): void;
-  encoder(encoder: Writer): void;
+  encode(encoder: Writer): void;
 }
