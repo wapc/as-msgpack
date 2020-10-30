cc1fdb7e543a8759dcb2b5d37a4ff52b4bfa399d
Phil Kedy <phil.kedy@gmail.com>
2020-10-30 09:54:41 -0400

    Adding export for Codec

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,5 +2,6 @@ export * from "./decoder";
 export * from "./writer";
 export * from "./encoder";
 export * from "./sizer";
+export * from "./codec";
 export * from "./value";
 export * from "./datareader";
